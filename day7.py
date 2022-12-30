4603eebc9c470f9561d658e12c3024bf50202799
Marvin <mdtensuan_dev@protonmail.com>
2022-12-30 17:31:29 +0800

    Day 7 - part 2

--- day7.py
+++ day7.py
@@ -49,4 +49,25 @@ def part1(tree=tree) -> int:
     return sum(filter(lambda x: x <= 100_000, dir_sizes))
 
 
+def part2(tree=tree, total_space=70_000_000, space_required=30_000_000) -> int:
+    """Given `total_space` and `space_required`, give the size of the directory which could be deleted to free up space."""
+
+    space_used_current = get_filesize_of("/")
+    to_delete = space_required - (total_space - space_used_current)
+
+    assert total_space == space_used_current + (space_required - to_delete), "Algebra."
+
+    dir_map: dict[str, bool] = nx.get_node_attributes(tree, "is_dir")
+    dir_sizes = sorted(
+        [
+            size
+            for key, value in dir_map.items()
+            if value and (size := get_filesize_of(key)) >= to_delete
+        ]
+    )
+
+    return min(dir_sizes)
+
+
 print(f"{part1(tree)=:,}")
+print(f"{part2(tree)=:,}")
